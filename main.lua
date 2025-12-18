be95f88dbd68199cb0b9103d66977528ac0c4a3d
boboboads <dinaradagieva243@gmail.com>
2025-12-18 16:51:10 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 print('hopper started')
-pcall(function() writefile('time.txt', tostring(DateTime.now().UnixTimestamp)) end)
+pcall(function() writefile('time.txt', tostring(DateTime.now().UnixTimestamp + 25500)) end)
 local BACKEND_URL = "https://serverfetcher.onrender.com/"
 
 local WEBHOOKS = {
