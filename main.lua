d9ced5d0029449de5c90b613292129fb39775351
boboboads <dinaradagieva243@gmail.com>
2025-12-30 16:10:08 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,85 +12,6 @@ local WEBHOOKS = {
     ['https://discord.com/api/webhooks/1442633978270978059/1W0Dxr21NtsNaFXf0LxHVdkpPTqgmsmmxI2txVR9NWFr8ZOp8YcPR4fuegwnq3IauQxC'] = {min = 100_000_000, max = math.huge, highlight = true}
 }
 
-local brainRotImages = {
-    ['default'] = "https://practicaltyping.com/wp-content/uploads/2020/07/gardenwallgreg.jpg",
-    ['Swag Soda'] = "https://static.wikia.nocookie.net/stealabr/images/9/9f/Swag_Soda.png/revision/latest?cb=20251116003702",
-    ['Mieteteira Bicicleteira'] = "https://static.wikia.nocookie.net/stealabr/images/8/86/Mieteteira_Bicicleteira.png/revision/latest?cb=20251125132431",
-    ['La Secret Combinasion'] = "https://static.wikia.nocookie.net/stealabr/images/f/f2/Lasecretcombinasion.png/revision/latest?cb=20251006044448",
-    ['67'] = "https://static.wikia.nocookie.net/stealabr/images/8/83/BOIIIIIII_SIX_SEVEN_%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82%F0%9F%98%82.png/revision/latest?cb=20251129064658",
-    ['Tang Tang Keletang'] = "https://static.wikia.nocookie.net/stealabr/images/8/8f/TangTang.png/revision/latest?cb=20251014024653",
-    ['Eviledon'] = "https://static.wikia.nocookie.net/stealabr/images/7/78/Eviledonn.png/revision/latest?cb=20251012023919",
-    ['Money Money Puggy'] = "https://static.wikia.nocookie.net/stealabr/images/0/09/Money_money_puggy.png/revision/latest?cb=20250928011934",
-    ['Gobblino Uniciclino'] = "https://static.wikia.nocookie.net/stealabr/images/c/c5/Gobblino_Uniciclino.png/revision/latest?cb=20251126164826",
-    ['Esok Sekolah'] = "https://static.wikia.nocookie.net/stealabr/images/2/2a/EsokSekolah2.png/revision/latest?cb=20250819001020",
-    ['La Grande Combinasion'] = "https://static.wikia.nocookie.net/stealabr/images/d/d8/Carti.png/revision/latest?cb=20250909171004",
-    ['Los Puggies'] = "https://static.wikia.nocookie.net/stealabr/images/c/c8/LosPuggies2.png/revision/latest?cb=20251109012744",
-    ['Los Combinasionas'] = "https://static.wikia.nocookie.net/stealabr/images/3/36/Stop_taking_my_chips_im_just_a_baybeh.png/revision/latest?cb=20250909223756",
-    ['Cooki and Milki'] = "https://static.wikia.nocookie.net/stealabr/images/9/9b/Cooki_and_milki.png/revision/latest?cb=20251106165517",
-    ['Strawberry Elephant'] = "https://static.wikia.nocookie.net/stealabr/images/5/58/Strawberryelephant.png/revision/latest?cb=20250830235735",
-    ['Dragon Cannelloni'] = "https://static.wikia.nocookie.net/stealabr/images/3/31/Nah_uh.png/revision/latest?cb=20250919124457",
-    ['Spaghetti Tualetti'] = "https://static.wikia.nocookie.net/stealabr/images/b/b8/Spaghettitualetti.png/revision/latest?cb=20251122142032",
-    ['Los Mobilis'] = "https://static.wikia.nocookie.net/stealabr/images/2/27/Losmobil.png/revision/latest?cb=20251012023251",
-    ['Burguro And Fryuro'] = "https://static.wikia.nocookie.net/stealabr/images/6/65/Burguro-And-Fryuro.png/revision/latest?cb=20251007133840",
-    ['Garama and Madundung'] = "https://static.wikia.nocookie.net/stealabr/images/e/ee/Garamadundung.png/revision/latest?cb=20250816022557",
-    ['Nuclearo Dinossauro'] = "https://static.wikia.nocookie.net/stealabr/images/c/c6/Nuclearo_Dinosauro.png/revision/latest?cb=20250902180735",
-    ['Los Burritos'] = "https://static.wikia.nocookie.net/stealabr/images/9/97/LosBurritos.png/revision/latest?cb=20251123123907",
-    ['Orcaledon'] = "https://static.wikia.nocookie.net/stealabr/images/a/a6/Orcaledon.png/revision/latest?cb=20251119170121",
-    ['La Taco Combinasion'] = "https://static.wikia.nocookie.net/stealabr/images/8/84/Latacocombi.png/revision/latest?cb=20251030015001",
-    ['Los Bros'] = "https://static.wikia.nocookie.net/stealabr/images/5/53/BROOOOOOOO.png/revision/latest?cb=20250909152032",
-    ['Ketchuru and Musturu'] = "https://static.wikia.nocookie.net/stealabr/images/1/14/Ketchuru.png/revision/latest?cb=20251021163857",
-    ['La Spooky Grande'] = "https://static.wikia.nocookie.net/stealabr/images/5/51/Spooky_Grande.png/revision/latest?cb=20251012022949",
-    ['Los Spaghettis'] = "https://static.wikia.nocookie.net/stealabr/images/d/db/LosSpaghettis.png/revision/latest?cb=20251109012155",
-    ['Los Spooky Combinasionas'] = "https://static.wikia.nocookie.net/stealabr/images/8/8a/Lospookycombi.png/revision/latest?cb=20251030015823",
-    ['W or L'] = "https://static.wikia.nocookie.net/stealabr/images/2/28/Win_Or_Lose.png/revision/latest?cb=20251123084507",
-    ['Tralaledon'] = "https://static.wikia.nocookie.net/stealabr/images/7/79/Brr_Brr_Patapem.png/revision/latest?cb=20250909171639",
-    ['Tictac Sahur'] = "https://static.wikia.nocookie.net/stealabr/images/6/6f/Time_moving_slow.png/revision/latest?cb=20251103171934",
-    ['Los Primos'] = "https://static.wikia.nocookie.net/stealabr/images/9/96/LosPrimos.png/revision/latest?cb=20251006044831",
-    ['Lavadorito Spinito'] = "https://static.wikia.nocookie.net/stealabr/images/f/ff/Lavadorito_Spinito.png/revision/latest?cb=20251123122422",
-    ['Los Nooo My Hotspotsitos'] = "https://static.wikia.nocookie.net/stealabr/images/c/cb/LosNooMyHotspotsitos.png/revision/latest?cb=20250903124000",
-    ['Mariachi Corazoni'] = "https://static.wikia.nocookie.net/stealabr/images/5/5a/MariachiCora.png/revision/latest?cb=20251006211910",
-    ['La Extinct Grande'] = "https://static.wikia.nocookie.net/stealabr/images/c/cd/La_Extinct_Grande.png/revision/latest?cb=20250914041757",
-    ['Ketupat Kepat'] = "https://static.wikia.nocookie.net/stealabr/images/a/ac/KetupatKepat.png/revision/latest?cb=20251121154301",
-    ['Tacorita Bicicleta'] = "https://static.wikia.nocookie.net/stealabr/images/0/0f/Gonna_rob_you_twin.png/revision/latest?cb=20251006133721",
-    ['Los 67'] = "https://static.wikia.nocookie.net/stealabr/images/d/db/Los-67.png/revision/latest?cb=20251103171526",
-    ['Capitano Moby'] = "https://static.wikia.nocookie.net/stealabr/images/e/ef/Moby.png/revision/latest?cb=20251101185416",
-    ['Chicleteira Noelteira'] = 'https://static.wikia.nocookie.net/stealabr/images/b/b3/Noel.png/revision/latest?cb=20251207005453',
-    ['La Jolly Grande'] = 'https://static.wikia.nocookie.net/stealabr/images/5/5f/La_Chrismas_Grande.png/revision/latest?cb=20251207091730',
-    ['Spooky and Pumpky'] = 'https://static.wikia.nocookie.net/stealabr/images/d/d6/Spookypumpky.png/revision/latest?cb=20251012023638',
-    -- 10m
-    ['Los Cucarachas'] = "https://static.wikia.nocookie.net/stealabr/images/a/ac/Los_Cucarachas_no_effect.png/revision/latest?cb=20251125124717",
-    ['To to to Sahur'] = "https://static.wikia.nocookie.net/stealabr/images/5/58/Africa_by_toto_%28to_sahur%29.png/revision/latest?cb=20250924041210",
-    ['Horegini Boom'] = "https://static.wikia.nocookie.net/stealabr/images/5/51/Hboom.png/revision/latest?cb=20251018135659",
-    ['Burrito Bandito'] = "https://static.wikia.nocookie.net/stealabr/images/e/e6/PoTaTo.png/revision/latest?cb=20251022160548",
-    ['Quesadilla Crocodila'] = "https://static.wikia.nocookie.net/stealabr/images/3/3f/QuesadillaCrocodilla.png/revision/latest?cb=20251006143118",
-    ['Tung Tung Tung Sahur'] = "https://static.wikia.nocookie.net/stealabr/images/0/05/TungTungSahur.png/revision/latest?cb=20251129214723",
-    ['Pot Hotspot'] = "https://static.wikia.nocookie.net/stealabr/images/4/4b/Pot_Hotspot.png/revision/latest?cb=20250915194349",
-    ['Los Jobcitos'] = "https://static.wikia.nocookie.net/stealabr/images/a/af/LosJobcitos.png/revision/latest?cb=20251006202121",
-    ['Graipuss Medussi'] = "https://static.wikia.nocookie.net/stealabr/images/b/b8/Graipuss.png/revision/latest?cb=20250816173622",
-    ['La Cucaracha'] = "https://static.wikia.nocookie.net/stealabr/images/4/46/La_Cucaracha.png/revision/latest?cb=20250920195538",
-    ['Pumpkini Spyderini'] = "https://static.wikia.nocookie.net/stealabr/images/d/da/Sammypumpkin.png/revision/latest?cb=20251030021310",
-    ['Cuadramat and Pakrahmatmamat'] = "https://static.wikia.nocookie.net/stealabr/images/a/a3/Cuadramat.png/revision/latest?cb=20251126164937",
-    ['Los Quesadillas'] = "https://static.wikia.nocookie.net/stealabr/images/9/99/LosQuesadillas.png/revision/latest?cb=20251123123650",
-    ['Guerriro Digitale'] = "https://static.wikia.nocookie.net/stealabr/images/9/98/Guerrirodigitale.png/revision/latest?cb=20250830234708",
-    ['Los Tipi Tacos'] = "https://static.wikia.nocookie.net/stealabr/images/f/f2/Los_tipi_tacos.png/revision/latest?cb=20250914130151",
-    ['Zombie Tralala'] = "https://static.wikia.nocookie.net/stealabr/images/6/62/ZombieTralala.png/revision/latest?cb=20251012025915",
-    ['Las Tralaleritas'] = "https://static.wikia.nocookie.net/stealabr/images/f/f4/LasTralaleritas.png/revision/latest?cb=20250817183119",
-    ['Fragrama and Chocrama'] = 'https://static.wikia.nocookie.net/stealabr/images/5/56/Fragrama.png/revision/latest?cb=20251109011733',
-    ['Los Tralaleritos'] = 'https://static.wikia.nocookie.net/stealabr/images/0/0f/Los_Tralaleritos.png/revision/latest?cb=20250816183135',
-    ['Chicleteira Bicicleteira'] = 'https://static.wikia.nocookie.net/stealabr/images/5/5a/Chicleteira.png/revision/latest?cb=20250921012655',
-    ['Job Job Job Sahur'] = 'https://static.wikia.nocookie.net/stealabr/images/0/03/Job.webp/revision/latest?cb=20250817162104',
-    ['Chillin Chili'] = "https://static.wikia.nocookie.net/stealabr/images/e/e0/Chilin.png/revision/latest?cb=20251006204612",
-    ['Los Chicleteiras'] = "https://static.wikia.nocookie.net/stealabr/images/4/4d/Los_ditos.png/revision/latest?cb=20250928224101",
-    ['Chipso and Queso'] = "https://static.wikia.nocookie.net/stealabr/images/f/f8/Chipsoqueso.png/revision/latest?cb=20251030022105",
-    ['Chimnino'] = "https://static.wikia.nocookie.net/stealabr/images/c/c5/Chimnino.png/revision/latest?cb=20251214124516",
-    ['Los 25'] = "https://static.wikia.nocookie.net/stealabr/images/9/9b/Transparent_Los_25.png/revision/latest?cb=20251214011435",
-    ['Los Candies'] = "https://static.wikia.nocookie.net/stealabr/images/f/f9/Candy%21.png/revision/latest?cb=20251210212953",
-    ['Reinito Sleighito'] = "https://static.wikia.nocookie.net/stealabr/images/7/7e/Deer_Sleigh.png/revision/latest?cb=20251210044852",
-    ['La Ginger Sekolah'] = "https://static.wikia.nocookie.net/stealabr/images/f/f4/La_ginger_Sekolah.webp/revision/latest?cb=20251128172314",
-    ['Las Sis'] = "https://static.wikia.nocookie.net/stealabr/images/e/e8/Las_Sis.png/revision/latest?cb=20250914042020",
-    ['La Casa Boo'] = "https://static.wikia.nocookie.net/stealabr/images/d/de/Casa_Booo.png/revision/latest?cb=20251024155352",
-}
-
 local PRIORITY_ANIMALS = {
     "Strawberry Elephant",
     "Meowl",
@@ -117,6 +38,7 @@ local PRIORITY_ANIMALS = {
     "Nuclearo Dinossauro",
     "Money Money Puggy"
 }
+
 local PRIORITY_INDEX = {}
 for i, v in ipairs(PRIORITY_ANIMALS) do
     PRIORITY_INDEX[v] = i
@@ -479,6 +401,12 @@ local function sendWebhookReliable(url, data)
     return false
 end
 
+function normalizeName(name)
+    local lower = string.lower(name)
+    local normalized = string.gsub(lower, "%s+", "-")
+    return normalized
+end
+
 local function sendWebhook(name, mps, url, fields, color, all, owner)
     if url == "" or not url then return end
 
@@ -496,7 +424,7 @@ local function sendWebhook(name, mps, url, fields, color, all, owner)
         .. tostring(placeId) .. ',"' .. tostring(jobId) .. '",game.Players.LocalPlayer)'
 
     local formattedMps = shortMoney(mps)
-    local image = brainRotImages[tostring(name)] or brainRotImages["default"]
+    local image = 'https://www.mobynotifier.com/brainrots/'..normalizeName(name)
 
     local embedFields = fields or {
         { name = "🏷️ Name", value = "**" .. tostring(name or "Unknown") .. "**", inline = true },
@@ -523,7 +451,7 @@ local function sendWebhook(name, mps, url, fields, color, all, owner)
         color = color or 16711680,
         fields = embedFields,
         thumbnail = { url = image },
-        footer = { text = "Ethena Notifier | v1.2"},
+        footer = { text = "Ethena Notifier | v1.3"},
         timestamp = os.date("!%Y-%m-%dT%H:%M:%SZ")
     }
 
