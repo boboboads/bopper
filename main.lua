d38b69b9dd964cbb6c1d65b5f9c4f7beec378876
boboboads <dinaradagieva243@gmail.com>
2026-04-19 17:39:56 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ print('hopper started')
 setfpscap(3)
 local BACKEND_URL = "https://serverfetcher.onrender.com/"
 local hop = 90
-local ver = "2.5"
+local ver = "2.6"
 
 
 local dc = false
@@ -369,6 +369,15 @@ local function tryTeleportTo(jobId)
     lastTeleportAt = os.clock()
     return ok
 end
+
+TeleportService.TeleportInitFailed:Connect(function()
+    lastFailAt = os.clock()
+    task.wait(0.6)
+    if rebirths.Value > 0 then
+        local nextId = nextServer()
+        if nextId then tryTeleportTo(nextId) end
+    end
+end)
 
 -- ==========================================================
 -- Utility
